f1184b5d52ca09c4c50e38c5406857ecb4e5bbb2
Nogonergade <93977206+Nogonergade@users.noreply.github.com>
2021-11-09 18:21:51 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,163 @@
+//  ____  _           _    _                    _
+// / ___|| | ___  ___| | _| |    ___   __ _  __| |
+// \___ \| |/ _ \/ _ \ |/ / |   / _ \ / _` |/ _` |
+//  ___) | |  __/  __/   <| |__| (_) | (_| | (_| |
+// |____/|_|\___|\___|_|\_\_____\___/ \__,_|\__,_|
+// version 2.17.1
+
+// Loading screen language
+//
+// You can create or edit languages by
+// editing/creating files in the languages folder
+//
+// Default available languages:
+// "english"
+// "brazilian_portuguese"
+config.language = "english"
+
+// Use an image for the logo instead of text
+config.logo_use_image = true
+
+// Image filename for the server logo
+// (Place the image file in the images folder)
+config.logo_image = "logo.png"
+
+// Text to be shown on logo
+config.logo_text = "Nogon's playground"
+
+// Use the server's name for the logo
+config.logo_servername = false
+
+// Center logo?
+// "true" or "false"
+config.logo_center = false
+
+// Display current map and gamemode?
+// "true" or "false"
+config.logo_show_info = true
+
+// Use a video for the background?
+// Otherwise it will use images
+// "true" or "false"
+config.background_use_video = false
+
+// Default available videos:
+// "blue_rays.webm"
+// "purple_blue_rays.webm"
+// "green_rays.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format
+// (Place the video file in the backgrounds/videos folder)
+config.background_video = ""
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// "true" or "false"
+config.background_map_based = false
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the image files in the backgrounds/images folder)
+config.background_images = [
+	"background1.jpg"
+]
+
+// (Image-background only)
+// Random background images order?
+// "true" or "false"
+config.background_images_random_order = false
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+config.background_images_duration = 5000
+
+// (Image-background only)
+// Image background fade duration
+// in milliseconds
+config.background_images_fade_duration = 2000
+
+// Enable background overlay?
+// "true" or "false"
+config.background_overlay = true
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+config.background_darkening = 30
+
+// Play music during the loading screen?
+// "true" or "false"
+config.music_enable = true
+
+// Display current music name?
+// "true" or "false"
+config.music_show = true
+
+// Music playlist
+// Add as many youtube ids/urls or audio files you want
+// (YouTube only works on Chromium beta)
+// (Use ogg files for compatibility with default Gmod)
+// (Place the audio files in the music folder)
+config.music_playlist = [
+	{
+		name: "Balming Tiger - Armadillo",
+		file: "tiger.ogg",
+	},
+	{
+		name: "Token - Doozy",
+		file: "Doozy.ogg",
+	},
+	{
+		name: "Omega Sapien - Pop The Tag",
+		file: "POP.ogg",
+	},
+	{
+		name: "Yonge Jaundice - Yellower",
+		file: "YELLOWER.ogg",
+	},
+]
+
+// Random music order?
+// "true" or "false"
+config.music_random_order = true
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+config.music_volume = 15
+
+// Use Gmod volume for the music instead of manual volume
+config.music_gmod_volume = true
+
+// Enable custom messages?
+// "true" or "false"
+config.messages_enable = true
+
+// Enter your custom messages below
+config.messages_list = [
+	"Nogon의 빌드서버에 오신걸 환영합니다.",
+	"필요하신 에드온이 있으면 말씀주세요.",
+	"즐거운 시간 되세요!",
+]
+
+// Random message order?
+// "true" or "false"
+config.messages_random_order = false
+
+// Delay between message changes
+// in milliseconds
+config.messages_delay = 5000
+
+// Messages fade duration
+// in milliseconds
+config.messages_fade_duration = 1000
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// "true" or "false"
+config.errors_show_ingame = true
---
+++ languages/english.js
@@ -0,0 +1,3 @@
+language.below_logo = "현재 맵은 %%map%% 입니다."
+language.loading = "로딩중입니다.."
+language.now_playing = "Now playing"
